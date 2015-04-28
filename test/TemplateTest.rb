fd01dec79c91117a349fafe862b99919c6dfc1e5
sigurdsvela <sigurdbergsvela@gmail.com>
2015-04-28 18:33:21 +0200

    Test Template class with sample templates

--- test/TemplateTest.rb
+++ test/TemplateTest.rb
@@ -3,7 +3,60 @@ require 'test_helper.rb'
 include Boot::Lib::Core
 
 describe Template do
-  
+
+  # Integration tests
+  describe "#initialize" do
+    test_dir = File.dirname(File.expand_path(__FILE__))
+    template_examples = test_dir + "/template-examples"
+
+    it "it does not throw on perfect template" do
+      Template.new template_examples + "/perfect"
+    end
+
+    it "does not pass json validation on invalid templates" do
+      # The json schema should validate to false on these
+      # tempaltes
+      assert_raises(InvalidTemplateException) {
+        Template.new template_examples + "/missing-description"
+      }
+
+      assert_raises(InvalidTemplateException) {
+        Template.new template_examples + "/missing-name"
+      }
+
+      assert_raises(InvalidTemplateException) {
+        Template.new template_examples + "/invalid-option"
+      }
+
+      assert_raises(InvalidTemplateException) {
+        Template.new template_examples + "/missing-flag-description"
+      }
+
+      assert_raises(InvalidTemplateException) {
+        Template.new template_examples + "/missing-option-description"
+      }
+
+      assert_raises(InvalidTemplateException) {
+        Template.new template_examples + "/missing-symbol-description"
+      }
+
+      assert_raises(InvalidTemplateException) {
+        Template.new template_examples + "/static-folders-invalid-type"
+      }
+    end
+
+    it "throws on invalid static folders" do
+      assert_raises(InvalidTemplateException) {
+        Template.new template_examples + "/static-folder-invalid"
+      }
+
+      assert_raises(InvalidTemplateException) {
+        Template.new template_examples + "/static-folders-invalid"
+      }
+    end
+  end
+
+
   describe "#structureFiles" do
     it "structures string" do
       assert_equal(
