7a2ca73b27522d42d09e7d9a6f03c4566e660938
sigurdsvela <sigurdbergsvela@gmail.com>
2015-04-30 23:23:41 +0200

    Checkout latest version be4 install in install.rb

--- install.rb
+++ install.rb
@@ -5,9 +5,24 @@ end
 puts "===> Downloading boot..."
 `git clone https://github.com/sigurdsvela/boot`
 Dir.chdir("boot") do
+  # Get the versions
+  `git fetch --tags`
+  
+  # Get the version
+  version = `git tag | sort -n | tail -1`
+  
+  # Remove newline form version
+  version = version[0..-2]
+  
+  puts "===> Installing latest version(#{version})"
+
+  # Checkout, send all output to /dev/null
+  system 'git checkout tags/#{version} &> /dev/null'
+
   puts "===> Downloading templates..."
   `git submodule init`
   `git submodule update`
+
   Dir.chdir("lib/templates") do
     `git submodule init`
     `git submodule update`
