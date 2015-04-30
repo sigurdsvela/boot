7a602e712cd2f2c3b14247f4d96b2660381c679b
sigurdsvela <sigurdbergsvela@gmail.com>
2015-04-30 21:40:28 +0200

    Format output in install.rb

--- install.rb
+++ install.rb
@@ -2,20 +2,20 @@ if (File.exists?('boot'))
   puts "Error: can not download into './boot'. Allready exists"
   exit()
 end
-puts "Downloading boot..."
+puts "===> Downloading boot..."
 `git clone https://github.com/sigurdsvela/boot`
 Dir.chdir("boot") do
-  puts "Downloading templates..."
+  puts "===> Downloading templates..."
   `git submodule init`
   `git submodule update`
   Dir.chdir("lib/templates") do
     `git submodule init`
     `git submodule update`
   end
-  puts "Building boot..."
+  puts "===> Building boot..."
   `rake gembuild`
-  puts "Installing boot..."
+  puts "===> Installing boot..."
   `rake geminstall`
-  puts "Done!"
-  puts "Success: run 'boot help' for list of commands"
+  puts "===> Done!"
+  puts "===> Success: run 'boot help' for list of commands"
 end
