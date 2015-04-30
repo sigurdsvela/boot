f1123d77eabe2d373070063ccb00f044ef715be9
sigurdsvela <sigurdbergsvela@gmail.com>
2015-04-30 21:39:24 +0200

    Replace `cd [...]` with Dir.chdir in install.rb
    -.-

--- install.rb
+++ install.rb
@@ -1,16 +1,17 @@
 puts "Downloading boot..."
 `git clone https://github.com/sigurdsvela/boot`
-`cd boot`
-puts "Downloading templates..."
-`git submodule init`
-`git submodule update`
-Dir.chdir("lib/templates") do
+Dir.chdir("boot") do
+  puts "Downloading templates..."
   `git submodule init`
   `git submodule update`
+  Dir.chdir("lib/templates") do
+    `git submodule init`
+    `git submodule update`
+  end
+  puts "Building boot..."
+  `rake gembuild`
+  puts "Installing boot..."
+  `rake geminstall`
+  puts "Done!"
+  puts "Success: run 'boot help' for list of commands"
 end
-puts "Building boot..."
-`rake gembuild`
-puts "Installing boot..."
-`rake geminstall`
-puts "Done!"
-puts "Run 'boot help' for list of commands"
